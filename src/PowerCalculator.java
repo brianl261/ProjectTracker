d44b15e213c9096697ed292fea85712247c47021
BrianLin <brianl261@nycstudents.net>
2026-01-14 10:00:58 -0500

    code implemented

--- src/PowerCalculator.java
+++ src/PowerCalculator.java
@@ -1,9 +1,26 @@
 import java.util.Scanner;
-    public class PowerCalculator {
-        public int MaxWatt(){
-
-            return 0;
+public class PowerCalculator {
+    Scanner Amp = new Scanner(System.in);
+        public int MaxAmp(int MaxAmp){
+            System.out.println("Enter the maximum allowed current draw");
+            MaxAmp = Amp.nextInt();
+            return MaxAmp;
         }
-    }
+        public int CurrentDraw(){
+            boolean DeviceStackComplete = false;
+            int nextDevice = 0;
+            int StackedAmps = 0;
+            while(DeviceStackComplete = false){
+                System.out.println("Please input the current draw of a device being added");
+                System.out.println("Type 0 to indicate you have added all devices");
+                nextDevice = Amp.nextInt();
+                StackedAmps += nextDevice;
+                if(nextDevice == 0){
+                    DeviceStackComplete = true;
+                }
+            }
+            return StackedAmps;
+        }
+}
 
 
